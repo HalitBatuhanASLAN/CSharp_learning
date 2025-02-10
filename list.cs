2be559931164b2b7d59e46cd65b831d30a3021dc
Halit Batuhan ASLAN <halitbatuhanaslan@gmail.com>
2025-02-10 22:33:12 +0300

    new topic examples added

---
+++ list.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace basics
+{
+    class Lists
+    {
+        /*public static void Main(string[] args)
+        {
+            List_metod();
+        }*/
+
+        private static void List_metod()
+        {
+            List<int> num_list = new List<int>();
+            num_list.Add(1);
+            num_list.Add(10);
+            int new_number = 15;
+            num_list.Add(new_number);
+            int[] arr = new int[] { 1, 2, 3, };
+            num_list.AddRange(arr);
+            num_list.Insert(0, new_number);
+            num_list.InsertRange(1, new int[] { 9, 8, 7 });
+            foreach (int i in num_list)
+            {
+                Console.Write($"{i,-5}");
+            }
+            num_list.RemoveAt(5);/*deletes number at index of 5*/
+            num_list.RemoveAt(num_list.IndexOf(8));/*deletes 8*/
+            Console.WriteLine();
+            foreach (int i in num_list)
+            {
+                Console.Write($"{i,-5}");
+            }
+        }
+    }
+}
---
+++ metods.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace basics
+{
+    public class Metods
+    {
+        public static void Main(string[] args)
+        {
+            welcomeMessage();
+            var result = addingNums(1, 15.98, 14.9, 156);
+            Console.WriteLine(result);
+            result = addingNums(-59.789654, 15679, -9998.12);
+            Console.WriteLine("{0,5:0.##}", result);
+
+            account(.15);
+            account(15, "abc");
+
+            int a = 10; int b = 20;
+            Console.WriteLine("{0},{1}", a, b);
+            change_number(ref a, ref b);
+            Console.WriteLine("{0},{1}", a, b);
+            Lambda_Usage();
+
+        }
+
+        private static void Lambda_Usage()
+        {
+            List<String> city1 = new List<String>
+            {
+                "Ankara","Istanbul","Adana","Antalya"
+            };
+            var city2 = city1;
+            //Lambda usage
+            city1.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            city2.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            /*de to referance when we add(remove) new city to a list it is automaticly added(removed) other list too*/
+            city1.Add("Bolu");
+            city1.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            city2.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            city2.Remove("Adana");
+            city1.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            city2.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void change_number(ref int a,ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        /// <summary>
+        /// we can give detail information about metods in here
+        /// </summary>
+        /// <param name="money"></param>
+        public static void account(double money)
+        {
+            Console.WriteLine($"Welcome customer you have {money}");
+        }
+        public static void account(double money,String name)
+        {
+            Console.WriteLine($"Welcome {name} you have {money}");
+        }
+
+        public static double addingNums(params double[] numbers)
+        {
+            double result = 0;
+            foreach (double number in numbers)
+            {
+                result += number;
+            }
+            return result;
+        }
+        public static void welcomeMessage()
+        {
+            Console.WriteLine("Welcome to our metod!");
+        }
+    }
+}
